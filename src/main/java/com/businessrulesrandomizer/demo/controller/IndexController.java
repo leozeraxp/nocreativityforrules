e89e460d32902f263927c331f31ac58feb03f81b
leozeraxp <70856911+leozeraxp@users.noreply.github.com>
2023-01-03 11:38:12 -0300

    ola mundo

--- src/main/java/com/businessrulesrandomizer/demo/controller/IndexController.java
+++ src/main/java/com/businessrulesrandomizer/demo/controller/IndexController.java
@@ -1,5 +1,14 @@
 package com.businessrulesrandomizer.demo.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class IndexController {
 
+	@GetMapping("/")
+	public ResponseEntity olaMundo() {
+		return ResponseEntity.ok("Olá Mundo");
+	}
 }
