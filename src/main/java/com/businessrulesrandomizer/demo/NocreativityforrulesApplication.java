163cb178547682f455dd56628ac8c2a1e4aa8cdd
leozeraxp <70856911+leozeraxp@users.noreply.github.com>
2023-01-03 11:12:57 -0300

    first commit

---
+++ src/main/java/com/businessrulesrandomizer/demo/NocreativityforrulesApplication.java
@@ -0,0 +1,13 @@
+package com.businessrulesrandomizer.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NocreativityforrulesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NocreativityforrulesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/businessrulesrandomizer/demo/NocreativityforrulesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.businessrulesrandomizer.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NocreativityforrulesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
