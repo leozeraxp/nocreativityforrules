0d9b69b2c856559d1fa0a41173d863b744b8bc18
Danilo Santos <danilocarsan@gmail.com>
2023-01-05 11:46:51 -0300

    docker

--- src/main/java/com/businessrulesrandomizer/demo/controller/auth/SignInController.java
+++ src/main/java/com/businessrulesrandomizer/demo/controller/auth/SignInController.java
@@ -3,12 +3,14 @@ package com.businessrulesrandomizer.demo.controller.auth;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("auth")
 public class SignInController {
 
-	@GetMapping("/auth/sign/in")
-	public ResponseEntity olaMundo() {
+	@GetMapping("/sign/in")
+	public ResponseEntity index() {
 		return ResponseEntity.ok("Sign in page");
 	}
 
--- src/main/java/com/businessrulesrandomizer/demo/controller/auth/SignUpController.java
+++ src/main/java/com/businessrulesrandomizer/demo/controller/auth/SignUpController.java
@@ -3,11 +3,13 @@ package com.businessrulesrandomizer.demo.controller.auth;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("auth")
 public class SignUpController {
 	
-	@GetMapping("auth/sign/up")
+	@GetMapping("/sign/up")
 	public ResponseEntity index() {
 		return ResponseEntity.ok("Sign up page");
 	}
